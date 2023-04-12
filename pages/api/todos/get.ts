d577429ca1bb7d1a4cf9a1538e8083025a378822
Jeffray Tipontipon <jeffray363@gmail.com>
2023-04-12 21:23:39 +0800

    feat: better implement of get todos

--- pages/api/todos/get.ts
+++ pages/api/todos/get.ts
@@ -1,14 +1,29 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+
 import { supabase } from '../../../utils/initSupabase'
+import { TodoType } from '@/helpers/common/types/todoType'
+import errorHandler from '@/helpers/api/errorHandler'
+import { ErrorResponse } from '@/helpers/common/types/errorResponse'
+import { checkRequestMethod } from '@/helpers/client/checkRequestMethod'
 
-const getTodos = async (req, res) => {
-  const { data: todos, error } = await supabase.from('todos').select('*')
+const getTodos = async (
+  req: NextApiRequest,
+  res: NextApiResponse<{ todos: TodoType[] } | ErrorResponse>,
+) => {
+  try {
+    const { data: todos, error } = await supabase.from('todos').select('*')
 
-  if (todos) {
-    return res.status(200).json({ todos })
-  }
+    checkRequestMethod(req.method)
 
-  if (error) {
-    return res.status(401).json({ error: { message: error.message } })
+    if (todos) {
+      return res.status(200).json({ todos })
+    }
+
+    if (error) {
+      return res.status(401).json({ error: { message: error.message } })
+    }
+  } catch (error) {
+    errorHandler(error, res)
   }
 }
 
