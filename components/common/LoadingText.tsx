563c3f833247b8086aafdc9360854a9cf433ce28
Jeffray Tipontipon <jeffray363@gmail.com>
2023-04-13 00:20:44 +0800

    feat: fetching todos loading indicator

---
+++ components/common/LoadingText.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LoadingText = () => {
+  return (
+    <div className="text-base font-medium text-gray-900">
+      Fetching todos ....
+    </div>
+  )
+}
+
+export default LoadingText
