4b0afec41081b4c674424965f30c5ed1d5d3bfbc
Jeffray Tipontipon <jeffray363@gmail.com>
2023-04-12 20:54:11 +0800

    improvement: better implementation of add todo api

--- pages/api/todos/add.ts
+++ pages/api/todos/add.ts
@@ -1,23 +1,51 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { MethodNotAllowed } from 'http-errors'
+
 import { supabase } from '../../../utils/initSupabase'
+import errorHandler from '@/helpers/api/errorHandler'
 
-const addTodo = async (req, res) => {
-  const { text } = req.body
+import { addTodoValidationSchema } from '@/helpers/common/validationSchema/addTodoSchema'
+import { TodoResponseType, TodoType } from '@/helpers/common/types/todoType'
+import { ErrorResponse } from '@/helpers/common/types/errorResponse'
+import { checkRequestMethod } from '@/helpers/client/checkRequestMethod'
 
-  if (!text) {
-    return res.status(401).json({ error: { message: 'must have text' } })
+interface CustomNextApiRequest extends NextApiRequest {
+  body: {
+    text: string
   }
+}
 
-  const { data: todo, error } = await supabase
-    .from('todos')
-    .insert([{ text }])
-    .select('*')
+const addTodo = async (
+  req: CustomNextApiRequest,
+  res: NextApiResponse<TodoResponseType | ErrorResponse>,
+) => {
+  try {
+    const { text } = req.body
 
-  if (todo) {
-    return res.status(200).json({ todo: todo[0] })
-  }
+    checkRequestMethod(req.method)
 
-  if (error) {
-    return res.status(401).json({ error: { message: error.message } })
+    const parsedResult = addTodoValidationSchema.parse({ text })
+
+    const { data: todo, error } = await supabase
+      .from('todos')
+      .insert([{ text: parsedResult.text }])
+      .select('*')
+
+    if (todo) {
+      return res.status(200).json({
+        todo: {
+          id: todo[0].id,
+          text: todo[0].text,
+          createdAt: todo[0].created_at,
+        },
+      })
+    }
+
+    if (error) {
+      return res.status(401).json({ error: { message: error.message } })
+    }
+  } catch (error) {
+    errorHandler(error, res)
   }
 }
 
